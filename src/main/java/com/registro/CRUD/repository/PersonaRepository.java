6389c91c6c02c6f48eaf142fef39cf969adf10c6
KevinCaballero00 <kevinarturocc@ufps.edu.co>
2025-10-14 18:42:12 -0500

    first commit

---
+++ src/main/java/com/registro/CRUD/repository/PersonaRepository.java
@@ -0,0 +1,7 @@
+package com.registro.CRUD.repository;
+
+import com.registro.CRUD.model.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+}
